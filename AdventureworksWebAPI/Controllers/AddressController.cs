607b93d1a2078262f2860d2f4e7581418cd101a2
Steve <scbuterb@gmail.com>
2023-09-08 22:19:53 -0400

    Added Documentataion XML and changed one method to Async

--- AdventureworksWebAPI/Controllers/AddressController.cs
+++ AdventureworksWebAPI/Controllers/AddressController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AdventureWorksWebAPI.Models;
+using System.ComponentModel;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,17 +18,33 @@ namespace AdventureWorksWebAPI.Controllers
             this._context = context;
         }
 
+        /// <summary>
+        /// Get an Address By Id Value
+        /// </summary>
+        /// <param name="addressId"></param>
+        /// <returns></returns>
         [HttpGet("{addressId}")]
+        [EndpointDescription("GetAddressById")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Address))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult GetAddressByID(int addressId)
+        public async Task<IActionResult> GetAddressByID(int addressId)
         {
-            var address = _context.Addresses.Find(addressId);
+            var address = await _context.Addresses.FindAsync(addressId);
+            if (address != null && address.StateProvinceId != 0)
+            {
+                address.StateProvince = await _context.StateProvinces.FindAsync(address.StateProvinceId).ConfigureAwait(true);
+            }
             return address == null ? NotFound() : Ok(address);
         }
 
-
+        /// <summary>
+        /// Gets an address by budinessEntityId and personId
+        /// </summary>
+        /// <param name="businessEntityId"></param>
+        /// <param name="personId"></param>
+        /// <returns></returns>
         [HttpGet("{businessEntityId}/{personId}")]
+        [Description("GetAddressByBusinessEntityContactId")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Address))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult GetAddressByBusinessEntityContactId(int businessEntityId, int personId)
@@ -55,7 +72,13 @@ namespace AdventureWorksWebAPI.Controllers
             return address == null ? NotFound() : Ok(address);
         }
 
+
+        /// <summary>
+        /// Returns list of all Address Types
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [Description("GetAddressTypes")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AddressType))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetAddressTypes()
--- AdventureworksWebAPI/Program.cs
+++ AdventureworksWebAPI/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using AdventureWorksWebAPI.Models;
 using System.Text.Json.Serialization;
-using System.Text.Json;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,7 +29,30 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddControllers().AddJsonOptions(
     x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
